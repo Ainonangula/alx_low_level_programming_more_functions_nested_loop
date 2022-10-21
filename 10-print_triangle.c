8b37ed1769dd73ef8d0ffe8037ea435978897b23
Ainonangula <114227170+Ainonangula@users.noreply.github.com>
2022-10-21 22:55:43 +0200

    Update 10-print_triangle.c

--- 10-print_triangle.c
+++ 10-print_triangle.c
@@ -1,1 +1,39 @@
 
+  #include "main.h"
+  #include <stdio.h>
+
+  /**
+  * print_triangle - print triangle, followed by a new line . 
+  * but for multiple of three prints fizz instead of the number 
+  * @size: size of the triangle
+  */
+  void print_triangle(int size) 
+ { 
+    if (size <= 0) 
+ {
+     _putchar('\n') 
+} 
+  else
+  {
+   int i, j;
+
+
+   for()
+ { 
+   printf("FizzBuzz");
+ {
+    else if (i == 1)
+ { 
+    printf("%d", i);
+ { 
+    else 
+{
+   printf("%d", i);
+ } 
+  } 
+    printf('\n') 
+      return (0);
+}
+        
+    
+
